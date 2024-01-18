5bef09502dc61a244454efba5924b2b167697a2c
띠드(대전_2반_정종욱) <139304030+ddideu@users.noreply.github.com>
2024-01-19 00:10:28 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+word = input()
+
+if word == "NLCS":
+  print("North London Collegiate School")
+elif word == "BHA":
+  print("Branksome Hall Asia")
+elif word == "KIS":
+  print("Korea International School")
+elif word == "SJA":
+  print("St. Johnsbury Academy")
