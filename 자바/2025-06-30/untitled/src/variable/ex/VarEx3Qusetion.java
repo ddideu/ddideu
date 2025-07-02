9183226e36bd102041e9fab38eca7b62e0a9e49c
정종욱 <cnhug3@naver.com>
2025-07-02 23:38:41 +0900

    자바 입문 변수 완료 2025-07-02

---
+++ 자바/2025-06-30/untitled/src/variable/ex/VarEx3Qusetion.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3Qusetion {
+    public static void main(String[] args) {
+        long longVer = 1000000000;
+        System.out.println(longVer);
+
+        boolean boolVer = true;
+        System.out.println(boolVer);
+    }
+}
